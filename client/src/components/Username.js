5e51dedc2bea66fe517c5e0439ec8dcd1cc92366
adarsh25t <adarsh25t@gmail.com>
2022-12-31 10:58:16 +0530

    create username page

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,8 +1,47 @@
 import React from 'react'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { useFormik} from 'formik';
+import { Toaster } from 'react-hot-toast';
+import { usernameValidate } from '../helper/validate';
 
 const Username = () => {
+
+    const formik = useFormik({
+        initialValues : {
+            username:''
+        },
+        validateOnBlur:false,
+        validateOnChange:false,
+        validate: usernameValidate,
+        onSubmit: async (value) => {
+            console.log(value);
+        }
+    })
+
+    const handlerSubmit = (e) => {
+        e.preventDefault();
+        formik.handleSubmit()
+    }
+
+
   return (
-    <div>Username</div>
+    <section className='home-page'>
+        <div className="container d-flex flex-column justify-content-center align-items-center h-100">
+            <Toaster position='top-center' reverseOrder={false}></Toaster>
+            <div className="home-page-card ">
+                <h3>Hello Again!</h3>
+                <p>Explore more by <br/> connecting with us</p>
+                <form className='d-flex flex-column justify-content-center align-items-center' onSubmit={handlerSubmit}>
+                    <AccountCircleIcon className='avatar'/>
+                    <input {...formik.getFieldProps('username')} type="text" placeholder='username...' />
+                    <Button variant='contained' type='submit'>Let's Go</Button>
+                    <p>Not a member <Link>Register Now</Link></p>
+                </form>
+            </div>
+        </div>
+    </section>
   )
 }
 
